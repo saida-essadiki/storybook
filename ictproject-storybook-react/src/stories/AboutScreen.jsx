9589912c44d0cd1b4ff3b67c0b022e0ecb44ffff
saida-essadiki <78084849+saida-essadiki@users.noreply.github.com>
2023-02-24 10:09:52 +0200

    Added about Us page

--- ictproject-storybook-react/src/stories/AboutScreen.jsx
+++ ictproject-storybook-react/src/stories/AboutScreen.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import About from "../about/About";
-import Footer from "../footer/Footer";
-import Banner from "../banner/Banner";
+import About from "./About";
+import Footer from "./Footer";
+import Banner from "./Banner";
 
 export function AboutScreen() {
   return (
