3665bdebf310a0e3149dad87ad11063069c36ee7
Muumz <kaisa.otterklau@gmail.com>
2023-03-02 16:45:09 +0200

    Banner changes
    banner image change, css fixing

--- ictproject-storybook-react/src/stories/Banner.js
+++ ictproject-storybook-react/src/stories/Banner.js
@@ -5,11 +5,7 @@ import './Banner.css';
 function Banner() {
   return (
     <div class="banner">
-      <div class="banner-text">
-        <h1>Flexbox Banner</h1>
-        <h3>Perfectly Centered</h3>
-        <h6>No matter how many lines of text you use.</h6>
-      </div>
+      
     </div>
   );
 }
