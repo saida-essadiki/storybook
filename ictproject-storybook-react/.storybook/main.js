b731ec62e5858b2fed30de31709e9e3ac8eb5445
Ephrem <39829776+ephrinaw@users.noreply.github.com>
2023-03-03 10:12:34 +0200

    mainjs
    mainjs interaction add on added

--- ictproject-storybook-react/.storybook/main.js
+++ ictproject-storybook-react/.storybook/main.js
@@ -1,17 +1,21 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    '../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    '@storybook/addon-a11y'
+    "@storybook/addon-a11y",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  features: {
+    interactionsDebugger: true, // 👈 Enable playback controls
   },
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+};
